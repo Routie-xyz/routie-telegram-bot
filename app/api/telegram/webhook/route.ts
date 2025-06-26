78f13a44249ad27db7dbbe5c79c073e71794766a
Nikita Sukhachev <traed.traed@mail.ru>
2025-06-26 13:15:53 +0700

    feat: stars payment

--- app/api/telegram/webhook/route.ts
+++ app/api/telegram/webhook/route.ts
@@ -1,48 +1,49 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 
-import { createTgBot, setAISybilHandlers } from "@/src/server/bots";
-import { TG_BOT_TOKEN, TG_WEBHOOK_SECRET } from "@/src/server/constants";
+import { createTgBot, setAISybilHandlers } from '@/src/server/bots';
+import { TG_BOT_TOKEN, TG_WEBHOOK_SECRET } from '@/src/server/constants';
 
 export const POST = async (req: NextRequest) => {
-  const bot = await createTgBot({ token: TG_BOT_TOKEN });
+    const bot = await createTgBot({ token: TG_BOT_TOKEN });
 
-  if (!bot) {
-    return NextResponse.json(
-      {
-        data: {
-          ok: false,
-          error: "Bot is not initialized",
-        },
-      },
-      { status: 400 }
+    if (!bot) {
+        return NextResponse.json(
+            {
+                data: {
+                    ok: false,
+                    error: 'Bot is not initialized',
+                },
+            },
+            { status: 400 }
+        );
+    }
+
+    const body = await req.json();
+
+    const telegramBotApiSecretToken = req.headers.get(
+        'x-telegram-bot-api-secret-token'
     );
-  }
 
-  const body = await req.json();
+    if (!TG_WEBHOOK_SECRET || telegramBotApiSecretToken !== TG_WEBHOOK_SECRET) {
+        return NextResponse.json(
+            {
+                data: {
+                    ok: false,
+                    error: 'Invalid secret token',
+                },
+            },
+            { status: 401 }
+        );
+    }
+    await bot.init();
 
-  const telegramBotApiSecretToken = req.headers.get(
-    "x-telegram-bot-api-secret-token"
-  );
-  if (!TG_WEBHOOK_SECRET || telegramBotApiSecretToken !== TG_WEBHOOK_SECRET) {
-    return NextResponse.json(
-      {
+    await setAISybilHandlers(bot);
+
+    await bot.handleUpdate(body);
+
+    return NextResponse.json({
         data: {
-          ok: false,
-          error: "Invalid secret token",
+            ok: true,
         },
-      },
-      { status: 401 }
-    );
-  }
-  await bot.init();
-
-  await setAISybilHandlers(bot);
-
-  await bot.handleUpdate(body);
-
-  return NextResponse.json({
-    data: {
-      ok: true,
-    },
-  });
+    });
 };
